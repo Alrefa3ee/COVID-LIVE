86a451d975bb3e6ea8c84e3af54dcf6543271afa
Alrefa3ee <abd516693@icloud.com>
2022-01-28 22:46:40 +0200

    first commit

---
+++ covid_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
+
+def home(request):
+    response = requests.get("https://www.worldometers.info/coronavirus/")
+    src = response.content
+    all1 = []
+    soup = BeautifulSoup(src, "lxml")
+    all = soup.findAll("div",{"id":"maincounter-wrap"})
+    for i in range(len(all)):
+        all1.append(all[i].find("span").text)
+    cases = str(all1[0])
+    deaths = str(all1[1])
+    recovered = str(all1[2])
+    context = {"cases":cases,
+               "deaths":deaths,
+               "recovered":recovered}
+    return render(request,"home.html",context)
